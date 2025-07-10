27660b2741e59767f5d69f0d597e93623df20c49
anurag-deore <anuragdeore125@gmail.com>
2025-07-11 03:16:04 +0530

    optimized 3440

--- 3440-RescheduleorMaxTime-II.py
+++ 3440-RescheduleorMaxTime-II.py
@@ -12,17 +12,39 @@ class Solution:
             gaps.append(startTime[j] - p)
             p = endTime[j]
         gaps.append(eventTime - p)
-        print("\n\nInitial gaps => ",gaps)
+        # print("\n\nInitial gaps => ",gaps)
         maxAns = 0
         for i in range(len(gaps)-1):
             maxAns = max(maxAns,gaps[i]+gaps[i+1])
-        print(maxAns)
+        # print(maxAns)
+
+        num_gaps = len(gaps)
+        prefix_max_gaps = [0] * num_gaps
+        prefix_max_gaps[0] = gaps[0]
+        for i in range(1, num_gaps):
+            prefix_max_gaps[i] = max(prefix_max_gaps[i - 1], gaps[i])
+
+        suffix_max_gaps = [0] * num_gaps
+        suffix_max_gaps[num_gaps - 1] = gaps[num_gaps - 1]
+        for i in range(num_gaps - 2, -1, -1):
+            suffix_max_gaps[i] = max(suffix_max_gaps[i + 1], gaps[i])
+
+        print(prefix_max_gaps)
+        print(suffix_max_gaps)
+
         for i in range(n):
             dur = endTime[i] - startTime[i]
-            if any(item >= dur for item in gaps[0:i]) or any(item >= dur for item in gaps[i+2:]):
-                print(f"max({maxAns},{gaps[i]+gaps[i+1]+dur})")
-                maxAns = max(maxAns,gaps[i]+gaps[i+1]+dur)
-            print("\nindex = ",i,"\n",dur,gaps[0:i],gaps[i+2:],"=> ",gaps[i],"+",gaps[i+1],"+",dur,"=",gaps[i]+gaps[i+1]+dur)
+            can_move = False
+            # Check for a slot in the gaps to the left
+            if i > 0 and prefix_max_gaps[i - 1] >= dur:
+                can_move = True
+            # Check for a slot in the gaps to the right
+            if not can_move and i + 2 < num_gaps and suffix_max_gaps[i + 2] >= dur:
+                can_move = True
+
+            if can_move:
+                maxAns = max(maxAns,  gaps[i] + dur + gaps[i + 1])
+            # print("\nindex = ",i,"\n",dur,gaps[0:i],gaps[i+2:],"=> ",gaps[i],"+",gaps[i+1],"+",dur,"=",gaps[i]+gaps[i+1]+dur)
         return maxAns
 
 
