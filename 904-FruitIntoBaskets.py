fe93453c606eff7ecd4cdf8cb92147e2bb59fb25
anurag-deore <anuragdeore125@gmail.com>
2025-08-05 00:48:02 +0530

    partially works 904

---
+++ 904-FruitIntoBaskets.py
@@ -0,0 +1,34 @@
+'''
+Problem Name: 904. Fruit Into Baskets
+Attempted : # 04-08-2025
+'''
+
+
+class Solution:
+    def totalFruit(self, fruits: list[int]) -> int:
+        n = len(fruits)
+        if n < 2:
+            return n
+        b1 = fruits[0]
+        b2 = fruits[1]
+        i = 2
+        maxSum = 2
+        maxcollected = 0
+        while i < n:
+            # print( f'{b1},{b2} fruits[i] = {fruits[i]}, sum => {maxSum}\n')
+            if fruits[i] == b1 or fruits[i] == b2:
+                maxSum += 1
+            else:
+                b1 = fruits[i-1]
+                b2 = fruits[i]
+                maxSum = 2
+            i += 1
+            maxcollected = max(maxcollected, maxSum)
+        # print(b1, b2)
+        return maxcollected
+
+
+s = Solution()
+print(s.totalFruit([1, 2, 1]))
+print(s.totalFruit([0, 0, 1, 1]))
+print(s.totalFruit([0, 1, 4, 2, 3, 2, 2, 1]))
