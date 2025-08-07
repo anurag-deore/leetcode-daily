1b2bcd2dc21b7f6ff5a99663f49e669199ca2323
Anurag Deore <anuragdeore125@gmail.com>
2025-08-08 04:11:25 +0530

    solved 1114-PrintInOrder

---
+++ 1114-PrintInOrder.py
@@ -0,0 +1,30 @@
+'''
+Problem Name: 1114. Print in Order
+Attempted : # 08-08-2025
+'''
+class Foo:
+    def __init__(self):
+        self.first_done = threading.Event()
+        self.second_done = threading.Event()
+
+
+    def first(self, printFirst: 'Callable[[], None]') -> None:
+        
+        # printFirst() outputs "first". Do not change or remove this line.
+        printFirst()
+
+        self.first_done.set()
+
+
+    def second(self, printSecond: 'Callable[[], None]') -> None:
+        self.first_done.wait()
+        # printSecond() outputs "second". Do not change or remove this line.
+        printSecond()
+
+        self.second_done.set()
+
+
+    def third(self, printThird: 'Callable[[], None]') -> None:
+        self.second_done.wait()
+        # printThird() outputs "third". Do not change or remove this line.
+        printThird()
