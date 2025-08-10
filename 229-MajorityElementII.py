3bbb00a952ab339b16137d6286e6c02f9865562c
anurag-deore <anuragdeore125@gmail.com>
2025-08-10 17:34:56 +0530

    solved 229

--- 229-MajorityElementII.py
+++ 229-MajorityElementII.py
@@ -6,14 +6,32 @@ Attempted : # 10-08-2025
 
 class Solution:
     def majorityElement(self, nums: list[int]) -> list[int]:
-        s = []
+        # brute FORCE solution
+        # s = []
+        # n = len(nums)
+        # n3 = n/3
+        # for i in nums:
+        #     if i not in s:
+        #         if nums.count(i) > n3:
+        #             s.append(i)
+        # return list(s)
+
+        s = {}
         n = len(nums)
         n3 = n/3
-        for i in nums:
-            if i not in s:
-                if nums.count(i) > n3:
-                    s.append(i)
-        return list(s)
+        bigs = []
+        if (n < 2):
+            return nums
+        for j in nums:
+            if s.get(j) != None:
+                s[j] += 1
+                if (s[j] > n3 and j not in bigs):
+                    bigs.append(j)
+            else:
+                s[j] = 1
+                if (s[j] > n3 and j not in bigs):
+                    bigs.append(j)
+        return bigs
 
 
 s = Solution()
